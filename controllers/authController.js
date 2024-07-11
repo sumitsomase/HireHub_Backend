c8d60677f9f5bd0c38a6ae019702247ed744ec85
sumitsomase <119779633+sumitsomase@users.noreply.github.com>
2024-07-11 08:47:53 +0530

    change in user.js file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 //const bcrypt = require('bcrypt');
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const User = require('../models/user');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 
