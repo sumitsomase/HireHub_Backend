665083b563bea74aa986722c655b7192f052bdf6
sumitsomase <119779633+sumitsomase@users.noreply.github.com>
2024-07-11 08:30:01 +0530

    Rebuild

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
